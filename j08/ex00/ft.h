c6db28906f7ce0488251dfb509678f251e722b86
Eddy Naboulet <eddy.naboulet.pro@gmail.com>
2015-10-19 13:21:35 +0200

    Update j08/ex00/ft.h
    - j08/ex00/ft.h
    
    Reconstructed from 42 file header Updated: 2015/10/19 13:21:35.

---
+++ j08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.H                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enaboule <enaboule@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/19 13:21:08 by enaboule          #+#    #+#             */
+/*   Updated: 2015/10/19 13:21:35 by enaboule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int*b);
+
+#endif
