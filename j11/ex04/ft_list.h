4ccd7de1da8b136037edf7189a45772c6d442ece
Eddy Naboulet <eddy.naboulet.pro@gmail.com>
2015-10-28 11:43:42 +0100

    Update j11/ex04/ft_list.h
    - j11/ex04/ft_list.h
    
    Reconstructed from 42 file header Updated: 2015/10/28 11:43:42.

---
+++ j11/ex04/ft_list.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enaboule <enaboule@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/28 11:43:37 by enaboule          #+#    #+#             */
+/*   Updated: 2015/10/28 11:43:42 by enaboule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct		s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+t_list				*ft_create_elem(void *data);
+void				ft_list_push_back(t_list **begin_list, void *data);
+void				ft_list_push_front(t_list **begin_list, void *data);
+int					ft_list_size(t_list *begin_list);
+t_list				*ft_list_last(t_list *begin_list);
+
+#endif
