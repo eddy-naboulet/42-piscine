5851662fcb15b6fa184cf6c5ff38cba39aa26e73
Eddy Naboulet <eddy.naboulet.pro@gmail.com>
2015-11-03 12:04:43 +0100

    Update j13/ex06/ft_btree.h
    - j13/ex06/ft_btree.h
    
    Reconstructed from 42 file header Updated: 2015/11/03 12:04:43.

---
+++ j13/ex06/ft_btree.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enaboule <enaboule@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/03 12:04:42 by enaboule          #+#    #+#             */
+/*   Updated: 2015/11/03 12:04:43 by enaboule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+# include <stdlib.h>
+
+typedef struct		s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}					t_btree;
+
+#endif
